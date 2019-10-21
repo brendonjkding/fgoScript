7f87d895a9c2ebca421c3325ca5ab3d17706d7ec
brendon <brendonjkding@126.com>
2019-10-21 20:25:37 +0800

    autoupdate

---
+++ autoupdate.lua
@@ -0,0 +1,23 @@
+function autoupdate()
+    data = httpGet("https://raw.githubusercontent.com/brendonjkding/fgo_lua_test/master/lib_fgo.lua")
+    file=io.open("/var/touchelf/scripts/lib_fgo.lua","w")
+    io.output(file)
+    io.write(data)
+    io.close(file)
+    
+    data = httpGet("https://raw.githubusercontent.com/brendonjkding/fgo_lua_test/master/main.lua")
+    file=io.open("/var/touchelf/scripts/main.lua","w")
+    io.output(file)
+    io.write(data)
+    io.close(file)
+    
+    data = httpGet("https://raw.githubusercontent.com/brendonjkding/fgo_lua_test/master/UPDATE.md")
+    file=io.open("/var/touchelf/scripts/本次更新内容.lua","w")
+    io.output(file)
+    io.write(data)
+    io.close(file)
+end
+
+
+
+
